ee1a0323c869370604c7a7855f4d8ca3e173270b
Neville Li <neville.lyh@gmail.com>
2024-09-30 22:38:43 -0700

    Mark done when build completed

--- src/build.py
+++ src/build.py
@@ -93,6 +93,7 @@ def build(args: argparse.Namespace) -> None:
     subprocess.run(cmd, check=True)
 
     logger.info('Monobase build completed')
+    mark_done('/srv/r8/monobase')
 
 
 if __name__ == '__main__':
