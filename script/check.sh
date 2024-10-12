0fb89cc0c7344526df87af303fb60127ce5c76a3
Neville Li <neville.lyh@gmail.com>
2024-10-12 17:47:36 -0400

    Use check.sh in CI

--- script/check.sh
+++ script/check.sh
@@ -7,6 +7,12 @@ set -euo pipefail
 MONOBASE_PYTHON='3.12'
 
 cd "$(git rev-parse --show-toplevel)"
-uv tool run ruff check --fix
-uv tool run ruff format
+
+if [[ -z "${CI:-}" ]]; then
+    uv tool run ruff check --fix
+    uv tool run ruff format
+else
+    uv tool run ruff check
+    uv tool run ruff format --check
+fi
 uv run --python "$MONOBASE_PYTHON" --with mypy mypy src
