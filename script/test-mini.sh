5a575021f6448a01fedd41d937f476a756e0148e
Neville Li <neville.lyh@gmail.com>
2024-11-15 13:15:21 -0800

    Test user layer imports

--- script/test-mini.sh
+++ script/test-mini.sh
@@ -73,3 +73,44 @@ test -f 'user/.done'
 test -f 'requirements-cog.txt'
 test -f 'requirements-mono.txt'
 test -f 'requirements-user.txt'
+
+
+if [ "$fail" -gt 0 ]; then
+    exit 1
+fi
+
+read -r -d '' SCRIPT << EOF || :
+import sys, cog, torch
+assert sys.version.startswith('3.12.6'), f'sys.version is not 3.12.6: {sys.version}'
+assert cog.__file__.startswith('/srv/r8/monobase/cog'), f'cog is not pre-installed: {cog.__file__}'
+assert cog.__version__ == '0.11.3', f'cog.__version__ is not 0.11.3: {cog.__version__}'
+assert torch.__version__ == '2.4.1+cu124', f'torch.__version__ is not 2.4.1+cu124: {torch.__version__}'
+print('PASS: venv versions')
+EOF
+
+docker run --rm \
+    --volume "$PWD/src:/opt/r8/monobase" \
+    --volume "$PWD/monobase:/srv/r8/monobase" \
+    --env R8_COG_VERSION=0.11.3 \
+    --env R8_CUDA_VERSION=12.4 \
+    --env R8_CUDNN_VERSION=9 \
+    --env R8_PYTHON_VERSION=3.12 \
+    --env R8_TORCH_VERSION=2.4.1 \
+    monobase:latest \
+    python -c "$SCRIPT"
+
+read -r -d '' SCRIPT << EOF || :
+import datasets, mypy, yaml, requests
+print('PASS: user venv')
+EOF
+
+docker run --rm \
+    --volume "$PWD/src:/opt/r8/monobase" \
+    --volume "$PWD/monobase:/srv/r8/monobase" \
+    --env R8_COG_VERSION=0.11.3 \
+    --env R8_CUDA_VERSION=12.4 \
+    --env R8_CUDNN_VERSION=9 \
+    --env R8_PYTHON_VERSION=3.12 \
+    --env R8_TORCH_VERSION=2.4.1 \
+    monobase:latest \
+    python -c "$SCRIPT"
