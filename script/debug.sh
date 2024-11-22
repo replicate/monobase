e06f10d6bb8cc0e3a7e0443653895636c8bffed4
Dan Buch <dan@meatballhat.com>
2024-11-21 19:24:32 -0500

    Minor tweaks to debug script
    to allow specifying user and working directory via env vars.

--- script/debug.sh
+++ script/debug.sh
@@ -9,17 +9,25 @@ MONOBASE_PYTHON='3.13'
 cd "$(git rev-parse --show-toplevel)"
 
 # Build test requirements
-uv run --python "$MONOBASE_PYTHON" python -m monobase.update --environment test
+uv run --python "${MONOBASE_PYTHON}" python -m monobase.update --environment test
 
 # Build test PREFIX
 mkdir -p build/monobase build/cache
 
+: "${DEBUG_USER:="$(id -u):$(id -g)"}"
+: "${DEBUG_DIR:="/opt/r8"}"
+
+set -x
 exec docker run --rm -it \
     --hostname debug \
-    --user "$(id -u):$(id -g)" \
-    --workdir '/opt/r8' \
-    --volume "$PWD/src/monobase:/opt/r8/monobase" \
-    --volume "$PWD/build/monobase:/srv/r8/monobase" \
-    --volume "$PWD/build/cache:/var/cache/monobase" \
+    --user "${DEBUG_USER}" \
+    --workdir "${DEBUG_DIR}" \
+    --volume "${PWD}/src/monobase:/opt/r8/monobase" \
+    --volume "${PWD}/build/monobase:/srv/r8/monobase" \
+    --volume "${PWD}/build/cache:/var/cache/monobase" \
+    --env R8_CUDA_VERSION="${R8_CUDA_VERSION:-"12.4"}" \
+    --env R8_CUDNN_VERSION="${R8_CUDNN_VERSION:-"9"}" \
+    --env R8_PYTHON_VERSION="${R8_PYTHON_VERSION:-"3.12"}" \
+    --env R8_TORCH_VERSION="${R8_TORCH_VERSION:-"2.4.1"}" \
     monobase:latest \
     bash -l
