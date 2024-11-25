b0997688bbc437733bb0b7ced4b9a8e570cb65c0
Dan Buch <dan@meatballhat.com>
2024-11-25 17:24:04 -0500

    Add a docs-only shim in case of running `python -m monobase` (#21)
    (like I just did) that tells a human what they probably wanted to do
    instead.

---
+++ src/monobase/__main__.py
@@ -0,0 +1,21 @@
+import sys
+import textwrap
+
+if __name__ == '__main__':
+    print(
+        textwrap.dedent(
+            """\
+    Running monobase directly does not do anything 🙀.
+    You probably want one of these instead:
+
+        python -m monobase.build
+
+        python -m monobase.diff
+
+        python -m monobase.monogen
+
+        python -m monobase.update
+    """
+        )
+    )
+    sys.exit(2)
