7d41a8d230430ab29055a45c8a31fde6747277eb
Neville Li <neville.lyh@gmail.com>
2025-03-11 14:08:33 -0400

    Patch gen 16: add hf_transfer

--- src/monobase/monogen.py
+++ src/monobase/monogen.py
@@ -25,7 +25,7 @@ class MonoGen:
 # Packaging is needed for flash-attn, etc.
 # Explicitly declare them here
 # Versions are not pinned and we will use whatever Torch index has
-SEED_PKGS = ['pip', 'packaging', 'setuptools', 'wheel']
+SEED_PKGS = ['pip', 'packaging', 'setuptools', 'wheel', 'hf_transfer']
 
 TEST_MONOGENS: list[MonoGen] = [
     MonoGen(
