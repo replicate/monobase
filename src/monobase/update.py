fa4093ddeae10e84d06ce7091d24973d1ef61620
Neville Li <neville.lyh@gmail.com>
2025-03-13 20:51:18 -0400

    Add symlink to latest generation

--- src/monobase/update.py
+++ src/monobase/update.py
@@ -17,13 +17,12 @@ log = logging.getLogger(__name__)
 
 
 def update_generation(
-    args: argparse.Namespace, tmp: TemporaryDirectory, mg: MonoGen
+    args: argparse.Namespace, tmp: TemporaryDirectory, mg: MonoGen, latest: bool
 ) -> None:
     log.info(f'Updating monobase generation {mg.id}')
     suffix = '' if args.environment == 'prod' else f'-{args.environment}'
-    rdir = os.path.join(
-        os.path.dirname(__file__), f'requirements{suffix}', f'g{mg.id:05d}'
-    )
+    gid = f'g{mg.id:05d}'
+    rdir = os.path.join(os.path.dirname(__file__), f'requirements{suffix}', gid)
     os.makedirs(rdir, exist_ok=True)
 
     # Always include CPU Torch
@@ -38,27 +37,32 @@ def update_generation(
         if updated:
             venvs.append({'python': p, 'torch': t, 'cuda': c})
 
-    matrix = {
-        'id': mg.id,
-        'cuda_versions': list(mg.cuda.keys()),
-        'cudnn_versions': list(mg.cudnn.keys()),
-        'python_versions': list(mg.python.keys()),
-        'torch_versions': mg.torch,
-        'venvs': venvs,
-    }
-    project_dir = pathlib.Path(__file__).absolute().parent.parent.parent
-    if args.environment == 'prod':
+    if latest:
+        matrix = {
+            'id': mg.id,
+            'cuda_versions': list(mg.cuda.keys()),
+            'cudnn_versions': list(mg.cudnn.keys()),
+            'python_versions': list(mg.python.keys()),
+            'torch_versions': mg.torch,
+            'venvs': venvs,
+        }
+        project_dir = pathlib.Path(__file__).absolute().parent.parent.parent
         with open(os.path.join(project_dir, 'matrix.json'), 'w') as f:
             json.dump(matrix, f, indent=2)
+        dest = os.path.join(
+            os.path.dirname(__file__), f'requirements{suffix}', 'latest'
+        )
+        os.symlink(gid, dest)
 
 
 def update(args: argparse.Namespace) -> None:
     tmp = TemporaryDirectory(prefix='monobase-')
     gens = []
-    for mg in sorted(MONOGENS[args.environment], reverse=True):
+    for i, mg in enumerate(sorted(MONOGENS[args.environment], reverse=True)):
         if mg.id < args.min_gen_id or mg.id > args.max_gen_id:
             continue
-        update_generation(args, tmp, mg)
+        latest = args.environment == 'prod' and i == 0
+        update_generation(args, tmp, mg, latest)
         gens.append(mg.id)
 
     log.info(f'Monobase update completed: {sorted(gens)}')
