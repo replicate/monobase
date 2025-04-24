071567372c4635aaa774327d09ae4a7288fd2adf
Neville Li <neville.lyh@gmail.com>
2025-04-23 21:22:33 -0400

    Improve packaging doc & test

--- packages/test.sh
+++ packages/test.sh
@@ -2,8 +2,8 @@
 
 # Test wheel files produced by package scripts
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $(basename "$0") <REQUIREMENTS> <SCRIPT>"
+if [ $# -lt 1 ]; then
+    echo "Usage: $(basename "$0") <REQUIREMENTS> [SCRIPT]"
     exit 1
 fi
 
@@ -13,6 +13,11 @@ fi
 # shellcheck disable=SC1091
 source /opt/r8/monobase/activate.sh
 
-# Run the test script
-python3 -c "$2"
-echo "PASS: $2"
+if [ $# -eq 1 ]; then
+    # No script, start a shell
+    python3
+else
+    # Run the test script
+    python3 -c "$2"
+    echo "PASS: $2"
+fi
