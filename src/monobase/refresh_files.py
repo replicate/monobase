eee7e32802a8b4c3e092ba7512fa204cd72a5c7d
Nikhil Sinha <nikhil@replicate.com>
2025-05-23 14:16:35 -0700

    Fix tmp pull

--- src/monobase/refresh_files.py
+++ src/monobase/refresh_files.py
@@ -2,26 +2,29 @@
 
 import argparse
 import hashlib
+import json
 import os
 import re
 import shutil
 import subprocess
 import sys
-import time
-from datetime import datetime
+import urllib
+import urllib.request
 from http import HTTPStatus
 
-import requests
+MONOBASE_PREFIX = os.environ.get('MONOBASE_PREFIX', '/srv/r8/monobase')
+PGET_BIN = os.environ.get('PGET_BIN', os.path.join(MONOBASE_PREFIX, 'bin/pget-bin'))
+KNOWN_WEIGHTS_DIR = os.environ.get('KNOWN_WEIGHTS_DIR', '')
 
 parser = argparse.ArgumentParser('refresh_files')
 parser.add_argument('-f', '--file-stats-url', type=str)
 parser.add_argument('-u', '--upstream-url', type=str)
-parser.add_argument('-p', '--parent-dir', type=str)
-
-KNOWN_WEIGHTS_DIR_ENV_VAR = 'KNOWN_WEIGHTS_DIR'
 
 
 def find_pget_exe() -> str:
+    # PGET_BIN is executable
+    if os.path.isfile(PGET_BIN) and os.access(PGET_BIN, os.X_OK):
+        return PGET_BIN
     # Look for real executable in PATH
     for p in os.environ['PATH'].split(os.pathsep):
         f = shutil.which('pget', path=p)
@@ -31,13 +34,20 @@ def find_pget_exe() -> str:
     sys.exit(1)
 
 
-def main(url, upstream, parent_dir) -> None:
+def main(url, upstream) -> None:
     if not url:
         print('Misconfigured files URL, exiting')
         sys.exit(1)
-    resp = requests.get(url)
-    if resp.status_code != HTTPStatus.OK:
-        print(f'Failed request to {url}: {resp.text}; exiting')
+
+    try:
+        req = urllib.request.Request(url, method='GET')
+        resp = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        print(f'Failed request to {url}: {e.read().decode()}; exiting')
+        sys.exit(1)
+
+    if resp.status != HTTPStatus.OK:
+        print(f'Failed request to {url}: {resp.read().decode()}; exiting')
         sys.exit(1)
 
     # The expectation for response from the file-stats-url is that
@@ -49,46 +59,32 @@ def main(url, upstream, parent_dir) -> None:
     #     "...",
     #   ]
     # }
-    body = resp.json()
+    body = json.loads(resp.read().decode())
     if 'files' not in body:
         print(f'Malformatted response: {body}; exiting')
         sys.exit(1)
 
-    old_dir = os.environ.get(KNOWN_WEIGHTS_DIR_ENV_VAR, '')
-
-    # Create a new directory to be the known_files_directory
-    now = datetime.now()
-    m = hashlib.sha256(now.strftime('%Y-%m-%d-%H:%M:%S').encode())
-    new_dir = os.path.join(parent_dir, m.hexdigest())
-    os.makedirs(new_dir)
     p = find_pget_exe()
     # pget each of the files into the new directory
     for file in body['files']:
         h = hashlib.sha256(file.encode())
-        old_path = os.path.join(old_dir, h.hexdigest())
-        new_path = os.path.join(new_dir, h.hexdigest())
-        # Copy instead of pget if the old path already exists
-        if os.path.exists(old_path):
-            shutil.copy(old_path, new_path)
-        else:
-            # If we have an upstream, make the URL <upstream>/<file> but strip http(s)://
-            # from the <file> URL
-            if upstream:
-                if file.startswith('http'):
-                    file = re.sub(r'https?:\\', '', file)
-                file = f'{upstream}/{file}'
-            subprocess.run([p, file, new_path])
-
-    # Switch the old env var
-    os.environ[KNOWN_WEIGHTS_DIR_ENV_VAR] = new_dir
-
-    # Remove old cache dir after 10 minutes to make sure nothing is reading from
-    # those files anymore
-    if old_dir:
-        time.sleep(600)
-        os.rmdir(old_dir)
+        # If we have an upstream, make the URL <upstream>/<file> but strip http(s)://
+        # from the <file> URL
+        if upstream:
+            if file.startswith('http'):
+                file = re.sub(r'https?:\\', '', file)
+            file = f'{upstream}/{file}'
+        try:
+            subprocess.run([p, file, os.path.join('/tmp', h.hexdigest())])
+            shutil.move(
+                os.path.join('/tmp', h.hexdigest()),
+                os.path.join(KNOWN_WEIGHTS_DIR, h.hexdigest()),
+            )
+        except Exception as e:
+            print(f'Error downloading {file}: {e}')
+            # Continue on anyways to get the rest of the files
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.file_stats_url, args.upstream_url, args.parent_dir)
+    main(args.file_stats_url, args.upstream_url)
