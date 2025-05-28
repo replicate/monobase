f5162e6a4810245ac8b1f8caba11e7d5ecb78b3f
Neville Li <neville.lyh@gmail.com>
2025-05-28 16:10:07 -0400

    Remove structlog

--- src/monobase/util.py
+++ src/monobase/util.py
@@ -14,9 +14,6 @@ import sys
 from dataclasses import dataclass
 from typing import Iterable
 
-import structlog
-from structlog.typing import EventDict
-
 HERE = os.path.dirname(os.path.abspath(__file__))
 IN_KUBERNETES = os.environ.get('KUBERNETES_SERVICE_HOST') is not None
 NODE_FEATURE_LABEL_FILE = '/etc/kubernetes/node-feature-discovery/features.d/monobase'
@@ -179,69 +176,13 @@ def du(d: str) -> None:
     subprocess.run(['du', '-ch', '-d', '1', d], check=True)
 
 
-def replace_level_with_severity(
-    _: logging.Logger, __: str, event_dict: EventDict
-) -> EventDict:
-    """
-    Replace the level field with a severity field as understood by Stackdriver
-    logs.
-    """
-    if 'level' in event_dict:
-        event_dict['severity'] = event_dict.pop('level').upper()
-    return event_dict
-
-
 def setup_logging() -> None:
-    # Switch to human-friendly log output if LOG_FORMAT environment variable is
-    # set to "development".
-    development_logs = os.environ.get('LOG_FORMAT', '') == 'development'
-
-    processors: list[structlog.types.Processor] = [
-        structlog.contextvars.merge_contextvars,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
-        structlog.processors.StackInfoRenderer(),
-        structlog.processors.TimeStamper(fmt='iso'),
-    ]
-
-    if development_logs:
-        # In development, set `exc_info` on the log event if the log method is
-        # `exception` and `exc_info` is not already set.
-        #
-        # Rendering of `exc_info` is handled by ConsoleRenderer.
-        processors.append(structlog.dev.set_exc_info)
-    else:
-        # Outside of development mode `exc_info` must be set explicitly when
-        # needed, and is translated into a formatted `exception` field.
-        processors.append(structlog.processors.format_exc_info)
-        # Set `severity`, not `level`, for compatibility with Google
-        # Stackdriver logging expectations.
-        processors.append(replace_level_with_severity)
-
-    # Stackdriver logging expects a "message" field, not "event"
-    processors.append(structlog.processors.EventRenamer('message'))
-
-    structlog.configure(
-        processors=processors
-        + [structlog.stdlib.ProcessorFormatter.wrap_for_formatter],
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        cache_logger_on_first_use=True,
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        '%(asctime)s\t%(levelname)s\t%(filename)s:%(lineno)d\t%(message)s'
     )
-
-    if development_logs:
-        log_renderer = structlog.dev.ConsoleRenderer(event_key='message')  # type: ignore
-    else:
-        log_renderer = structlog.processors.JSONRenderer()  # type: ignore
-
-    formatter = structlog.stdlib.ProcessorFormatter(
-        foreign_pre_chain=processors,
-        processors=[
-            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-            log_renderer,
-        ],
-    )
-
-    handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
     root = logging.getLogger()
