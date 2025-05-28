1009f9db566d6f5f3136d975b8b6d46ea280d281
Neville Li <neville.lyh@gmail.com>
2025-05-28 14:30:47 -0400

    Tweak refresh_files.py

--- src/monobase/refresh_files.py
+++ src/monobase/refresh_files.py
@@ -3,26 +3,31 @@
 import argparse
 import hashlib
 import json
+import logging
 import os
-import re
 import shutil
 import subprocess
 import sys
+import time
 import urllib
+import urllib.error
 import urllib.parse
 import urllib.request
 from http import HTTPStatus
 
+from monobase.util import setup_logging
+
 MONOBASE_PREFIX = os.environ.get('MONOBASE_PREFIX', '/srv/r8/monobase')
 PGET_BIN = os.environ.get('PGET_BIN', os.path.join(MONOBASE_PREFIX, 'bin/pget-bin'))
-PGET_KNOWN_WEIGHTS_DIR = os.environ.get('PGET_KNOWN_WEIGHTS_DIR', '')
 
 parser = argparse.ArgumentParser('refresh_files')
-parser.add_argument('-q', '--query-url', type=str, required=True)
-parser.add_argument('-q', '--query-id', type=str, required=True)
-parser.add_argument('-u', '--upstream-url', type=str)
-parser.add_argument('-a', '--auth-token', type=str, required=True)
-parser.add_argument('-m', '--max-size', type=int, required=True)
+parser.add_argument('--query-id', type=str, required=True)
+parser.add_argument('--auth-token', type=str, required=True)
+parser.add_argument('--weights-dir', type=str, required=True)
+parser.add_argument('--max-size', type=int, default=1024 * 1024 * 1024 * 1024)  # 1TiB
+parser.add_argument('--sleep-interval', type=int, default=60 * 60 * 24)  # 24 hours
+
+log = logging.getLogger(__name__)
 
 
 def find_pget_exe() -> str:
@@ -34,45 +39,46 @@ def find_pget_exe() -> str:
         f = shutil.which('pget', path=p)
         if f is not None and f != __file__:
             return f
-    print('Cannot find pget executable', file=sys.stderr)
+    log.error('Cannot find pget executable')
     sys.exit(1)
 
 
-def main(query_url, query_id, upstream, auth_token, max_size) -> None:
-    try:
-        create_req = urllib.request.Request(
-            query_url,
-            method='POST',
-            headers={'X-Honeycomb-Team': auth_token},
-            data=urllib.parse.urlencode(
-                {
-                    'query_id': query_id,
-                    'disable_series': True,
-                    'disable_total_by_aggregate': True,
-                    'disable_other_by_aggregate': True,
-                }
-            ).encode(),
-        )
-        create_resp = urllib.request.urlopen(create_req)
+def sync(args: argparse.Namespace) -> None:
+    create_req = urllib.request.Request(
+        'https://api.honeycomb.io/1/query_results/hermes',
+        method='POST',
+        headers={
+            'Content-Type': 'application/json',
+            'X-Honeycomb-Team': args.auth_token,
+        },
+        data=json.dumps(
+            {
+                'query_id': args.query_id,
+                'disable_series': True,
+                'disable_total_by_aggregate': True,
+                'disable_other_by_aggregate': True,
+            }
+        ).encode('utf-8'),
+    )
+    create_resp = urllib.request.urlopen(create_req)
+    assert create_resp.status >= 200 and create_resp.status < 300, (
+        f'failed to create query results: {create_resp.status}'
+    )
+    create_body = json.loads(create_resp.read().decode())
+    assert 'id' in create_body, (
+        f'missing id in create query results response: {create_body}'
+    )
 
-        create_body = json.loads(create_resp.read().decode())
-        if 'id' not in create_body:
-            print(f'Malformatted response from create endpoint: {create_body}; exiting')
-            sys.exit(1)
-
-        req = urllib.request.Request(
-            urllib.parse.urljoin(query_url, create_body['id']),
-            method='GET',
-            headers={'X-Honeycomb-Team': auth_token},
-        )
-        resp = urllib.request.urlopen(req)
-    except urllib.error.HTTPError as e:
-        print(f'Failed request to {query_url}: {e.read().decode()}; exiting')
-        sys.exit(1)
-
-    if resp.status != HTTPStatus.OK:
-        print(f'Failed request to {query_url}: {resp.read().decode()}; exiting')
-        sys.exit(1)
+    req = urllib.request.Request(
+        f'https://api.honeycomb.io/1/query_results/hermes/{create_body["id"]}',
+        method='GET',
+        headers={
+            'Content-Type': 'application/json',
+            'X-Honeycomb-Team': args.auth_token,
+        },
+    )
+    resp = urllib.request.urlopen(req)
+    assert resp.status == HTTPStatus.OK, 'failed to get query result'
 
     # The expectation for response from the file-stats-url is that
     # it is a GET request that returns a list of files, like
@@ -91,64 +97,70 @@ def main(query_url, query_id, upstream, auth_token, max_size) -> None:
     # }
     body = json.loads(resp.read().decode())
     results = body.get('data', {}).get('results', [])
-    if len(results) == 0:
-        print(f'Malformatted response: {body}; exiting')
-        sys.exit(1)
+    assert len(results) >= 0, 'empty query results'
 
-    file_set = set([])
+    files: dict[str, str] = {}
     total_size = 0
 
     # Parse results to figure out what files to download
     for result in results:
         data = result.get('data', {})
         if 'cache.request.resolved_url' not in data:
-            print(f'Malformatted result: {result}; continuing')
+            log.error('Missing object URL in query result: %s', result)
             continue
-        file = data['cache.request.resolved_url']
-        h = hashlib.sha256(file.encode())
-        file_set.add(h.hexdigest())
 
         # Check we won't violate the total size by downloading this file
         if 'cache.response.object_size' not in data:
-            print(f'Malformatted result: {result}; continuing')
+            log.error('Missing object size in query result: %s', result)
             continue
         size = data['cache.response.object_size']
-        if (total_size + size) > max_size:
-            print(f'Downloading file would be violate size limit, skipping file {file}')
-            continue
+        if (total_size + size) > args.max_size:
+            log.info('Max size reached: %d files, %d bytes', len(files), total_size)
+            break
         total_size += size
 
+        url = data['cache.request.resolved_url']
+        h = hashlib.sha256(url.encode()).hexdigest()
+        files[h] = url
+        log.info('%s %12d %s', h, size, url)
+
+    os.makedirs(args.weights_dir, exist_ok=True)
+
     # Delete files that should no longer be here so we keep the directory clean
-    for file in os.listdir(PGET_KNOWN_WEIGHTS_DIR):
-        if file not in file_set:
-            os.remove(os.path.join(PGET_KNOWN_WEIGHTS_DIR, file))
+    for file in os.listdir(args.weights_dir):
+        if file not in files:
+            log.info('Deleting stale file: %s', file)
+            os.remove(os.path.join(args.weights_dir, file))
 
     p = find_pget_exe()
     # pget each of the files into the new directory
-    for file in file_set:
-        # If we have an upstream, make the URL <upstream>/<file> but strip http(s)://
-        # from the <file> URL
-        if upstream:
-            if file.startswith('http'):
-                file = re.sub(r'https?://', '', file)
-            file = f'{upstream}/{file}'
+    for h, url in files.items():
         try:
             # Download to tmp directory, then move into PGET_KNOWN_WEIGHTS_DIR directory
             subprocess.run(
-                [p, file, os.path.join(PGET_KNOWN_WEIGHTS_DIR, 'tmp', h.hexdigest())],
+                [p, url, os.path.join(args.weights_dir, f'tmp-{h}')],
                 check=True,
             )
             shutil.move(
-                os.path.join(PGET_KNOWN_WEIGHTS_DIR, 'tmp', h.hexdigest()),
-                os.path.join(PGET_KNOWN_WEIGHTS_DIR, h.hexdigest()),
+                os.path.join(args.weights_dir, f'tmp-{h}'),
+                os.path.join(args.weights_dir, h),
             )
         except Exception as e:
-            print(f'Error downloading {file}: {e}')
+            log.error(f'Error downloading {url}: {e}')
             # Continue on anyways to get the rest of the files
 
 
+def main(args: argparse.Namespace) -> None:
+    while True:
+        try:
+            sync(args)
+        except Exception as e:
+            log.error('Sync failed: %s', e)
+        finally:
+            log.info('Sleeping for %d seconds', args.sleep_interval)
+            time.sleep(args.sleep_interval)
+
+
 if __name__ == '__main__':
-    args = parser.parse_args()
-    main(
-        args.query_url, args.query_id, args.upstream_url, args.auth_token, args.max_size
-    )
+    setup_logging()
+    main(parser.parse_args())
